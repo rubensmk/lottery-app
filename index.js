e502f74ab4d4c617201e531ac6f523d475c2c915
rubensmk <rubenskishimoto@gmail.com>
2021-06-08 18:54:33 -0300

    feat: CompleteGame, ClearGame, AddCart and Remove from Cart Function

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
 const ajax = new XMLHttpRequest();
 const numbers = document.getElementById('bet-numbers');
+const cardList = document.getElementById('card-list');
 let data = []
 let selectedNumber = []
 
 const Form = {
     number: document.querySelector('input#number'),
-    submit(index, maxNum) {
+
+    select(index, maxLimit) {
         let newEntry = index + 1
-        if (selectedNumber.includes(newEntry) || selectedNumber.length >= maxNum) {
-            console.log('repetido')
+
+        if (selectedNumber.length >= maxLimit) {
+            alert(`Já foram selecionados o número limite do jogo: ${maxLimit}, finalize adicionando ao carrinho. 🛒 `)
         } else {
             selectedNumber.push(newEntry)
+            number[index].disabled = true
         }
         console.log(selectedNumber)
+    },
+
+    clearGame() {
+        App.reload()
+    },
+
+    completeGame() {
+        const min = 1;
+        const max = 25;
+
+        for (let i = 0; i < 15; i++) {
+            let randomNum = Math.floor(Math.random() * max) + min;
+            let check = selectedNumber.includes(randomNum);
+
+            if (check === false) {
+                selectedNumber.push(randomNum);
+                number[randomNum - 1].disabled = true
+            } else {
+                while (check === true) {
+                    randomNum = Math.floor(Math.random() * max) + min;
+                    check = selectedNumber.includes(randomNum);
+                    if (check === false) {
+                        selectedNumber.push(randomNum);
+                        number[randomNum - 1].disabled = true
+                    }
+                }
+            }
+        }
+        console.log(selectedNumber)
+    },
+
+    submit() {
+
+        const spanNumbers = selectedNumber.sort((a, b) => a - b).join()
+
+        cardList.innerHTML = `
+                            <div class="cart-card" id="card">
+                                <img src="assets/trash.svg" class="cart-card-icon" onclick="Form.delete()"/>
+                                    <div class="cart-card-content-lotofacil">
+                                        <span class="cart-card-numbers">
+                                        ${spanNumbers}
+                                        </span>
+                                        <div class="cart-card-game">
+                                        <strong class="cart-card-lotofacil">Lotofácil</strong>
+                                        <p class="cart-card-price">R$ 2,50</p>
+                                        </div>
+                                    </div>
+                            </div>
+                            `
+    },
+
+    delete() {
+        const card = document.getElementById('card')
+        cardList.removeChild(card)
+        App.reload()
     }
 }
 
@@ -31,11 +90,13 @@ const getGames = {
                 document.getElementById('description').innerHTML = data.description
 
                 numbers.innerHTML = ''
+                selectedNumber = []
 
                 for (let index = 0; index < data.range; index++) {
-                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.submit(${index}, ${data['max-number']})">`
+                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.select(${index}, ${data['max-number']})">`
                     numbers.innerHTML += html
                 }
+
             }
         })
 
@@ -54,11 +115,13 @@ const getGames = {
                 document.getElementById('description').innerHTML = data.description
 
                 numbers.innerHTML = ''
+                selectedNumber = []
 
                 for (let index = 0; index < data.range; index++) {
-                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.submit(${index})">`
+                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.select(${index}, ${data['max-number']})">`
                     numbers.innerHTML += html
                 }
+
             }
         })
 
@@ -77,9 +140,10 @@ const getGames = {
                 document.getElementById('description').innerHTML = data.description
 
                 numbers.innerHTML = ''
+                selectedNumber = []
 
                 for (let index = 0; index < data.range; index++) {
-                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.submit(${index})">`
+                    let html = `<input type="button" class="bet-number" value="${index + 1}" id="number" onclick="Form.select(${index}, ${data['max-number']})">`
                     numbers.innerHTML += html
                 }
             }
